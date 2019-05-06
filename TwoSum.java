f1606de4f0e849c577ca7da1f568f7fbb623599d
ruixiaoLee <45024569+ruixiaoLee@users.noreply.github.com>
2019-05-06 16:45:26 +0900

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,3 +1,4 @@
+//HashMap solution,Time complexity : O(n)
 public int[] twoSum(int[] nums, int target) {
     Map<Integer, Integer> map = new HashMap<>();
     for (int i = 0; i < nums.length; i++) {
