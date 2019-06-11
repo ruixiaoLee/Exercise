10eb4c88904af553f67fe947de74a6da485554a0
ruixiaoLee <45024569+ruixiaoLee@users.noreply.github.com>
2019-06-11 22:21:41 +0900

    Update PlusOne.cpp

--- PlusOne.cpp
+++ PlusOne.cpp
@@ -28,16 +28,16 @@ vector<int> plusOne(vector<int>& digits) {
 //from leetcode
 //非常简单了可以说..
 vector<int> plusOne2(vector<int>& digits) {
-		int len = digits.size(), carry=1;
-		while(carry && len--)//当carry=0且len=0跳出//注意本题+1刚好和carry=1一样了
-			if(digits[len] < 9) {   //0~8//carry默认是1，如果是0～8则更新carry成0
-				digits[len] += 1;
-				carry = 0;
-			}
-			else  digits[len] = 0;//当本位是9的时候推入0因为此时carry=1
-		if(carry)//while循环出已经算完全部的位如果此时依然有carry则要在前面插入1
-			digits.insert(digits.begin(), 1);
-		return digits;
+	int len = digits.size(), carry=1;
+	while(carry && len--)           //当carry=0且len=0跳出//注意本题+1刚好和carry=1一样了
+		if(digits[len] < 9) {   //0~8//carry默认是1，如果是0～8则更新carry成0
+			digits[len] += 1;
+			carry = 0;
+		}
+		else  digits[len] = 0;  //当本位是9的时候推入0因为此时carry=1
+	if(carry)                       //while循环出已经算完全部的位如果此时依然有carry则要在前面插入1
+		digits.insert(digits.begin(), 1);
+	return digits;
 }
 
 int main(void){
