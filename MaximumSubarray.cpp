f4a94a4f06109a96f9a2c8259628003f8286ec3d
ruixiaoLee <45024569+ruixiaoLee@users.noreply.github.com>
2019-06-11 18:20:35 +0900

    Update MaximumSubarray.cpp

--- MaximumSubarray.cpp
+++ MaximumSubarray.cpp
@@ -7,7 +7,7 @@ int maxSubArray(vector<int>& nums) {
     int dp=nums[0];//子串最大和
     int sum=dp;//全局最大和
     for(int i=1 ; i<nums.size() ; i++){
-        dp=max(dp+nums[i], nums[i]);//如果dp加上下一个数字比dp大则更新dp
+        dp=max(dp+nums[i], nums[i]);//更新dp为（dp加上下一个数字，下个数字本身大）中较大的
         sum=max(dp,sum);//如果子串最大和比全局大则更新全局
     }
     return sum;
